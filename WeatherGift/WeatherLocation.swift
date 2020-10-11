274cb4f38b6a464587f077a9cfb803180fbec154
Marissa D'Antonio <dantonma@bc.edu>
2020-10-11 15:23:14 -0400

    Completed -> 6.2

---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Marissa D'Antonio on 10/11/20.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name: String
+    var latitude: Double
+    var longitude: Double
+}
