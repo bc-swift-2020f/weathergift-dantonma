93ae02c15059cf5cd23d5a20b10824022607a602
Marissa D'Antonio <dantonma@bc.edu>
2020-10-13 21:28:50 -0400

    Completed -> Week 8

--- WeatherGift/LocationDetailViewController.swift
+++ WeatherGift/LocationDetailViewController.swift
@@ -14,6 +14,7 @@ class LocationDetailViewController: UIViewController {
     @IBOutlet weak var summaryLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
     
+    @IBOutlet weak var pageControll: UIPageControl!
     var weatherLocation: WeatherLocation!
     var locationIndex = 0
     
@@ -36,6 +37,9 @@ class LocationDetailViewController: UIViewController {
         temperatureLabel.text = "--°"
         summaryLabel.text = ""
         
+        pageControll.numberOfPages = pageViewController.weatherLocations.count
+        pageControll.currentPage = locationIndex
+        
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -53,6 +57,16 @@ class LocationDetailViewController: UIViewController {
         pageViewController.weatherLocations = source.weatherLocations
         pageViewController.setViewControllers([pageViewController.createLocationDetailViewController(forPage: locationIndex)], direction: .forward, animated: false, completion: nil)
         
+    }
+    @IBAction func pageControllTapped(_ sender: UIPageControl) {
+        let pageViewController = UIApplication.shared.windows.first!.rootViewController as! PageViewController
+        
+        var direction: UIPageViewController.NavigationDirection = .forward
+        if sender.currentPage < locationIndex {
+            direction = .reverse
+        }
+        
+        pageViewController.setViewControllers([pageViewController.createLocationDetailViewController(forPage: sender.currentPage)], direction: direction, animated: true, completion: nil)
     }
     
 }
